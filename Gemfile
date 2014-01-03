45c3c4a783530ff7b428bc81e7cac7aa50befa4e
Joe Gesualdo <joegesualdo@gmail.com>
2014-01-03 11:54:21 -0500

    Enforce available locales. English: fix annoying messag when running specs

--- Gemfile
+++ Gemfile
@@ -6,9 +6,10 @@ gem 'rails', '4.0.1'
 # Database ===============
 gem 'pg'
 
-# Authentication ========
+# Authentication =========
 gem 'devise'
 
+# Assets =================
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,9 @@ module CodePairs
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+ 
+    # Removes the annoying messages when specs are run.
+    # ex. http://stackoverflow.com/questions/20361428/rails-i18n-validation-deprecation-warning 
+    config.i18n.enforce_available_locales = true
   end
 end
