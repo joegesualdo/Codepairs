3adccadcf1279c468e63bd8631d121af391d64e0
Joe Gesualdo <joegesualdo@gmail.com>
2014-01-04 15:09:42 -0500

    Finish request feature; all tests passing

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,18 @@
 class RequestsController < ApplicationController
   def new
-    
+   @request = Request.new 
+  end
+
+  def create
+    @request = Request.new(request_params)
+    if @request.save
+      redirect_to :profile_users
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:description, :student_id)
   end
 end
--- spec/features/user_profiles_spec.rb
+++ spec/features/user_profiles_spec.rb
@@ -1,23 +1,5 @@
 require 'spec_helper'
 
-feature 'User profile' do
-  let(:user){ FactoryGirl.create(:user, password: 'passwordtest') }
-  
-  before do
-    visit root_path
-    click_link 'Log in'
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'passwordtest'
-    click_button 'Sign in'
-    click_link 'Profile' 
-  end
-  scenario 'is available' do
-    expect(page).to have_content user.email
-  end
-  scenario 'has first name' do
-    expect(page).to have_content user.first
-  end
-end
 
 feature 'Expert profile' do
   let(:expert){ FactoryGirl.create(:expert, password: 'passwordtest') }
