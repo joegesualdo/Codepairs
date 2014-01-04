0eb0a7bb7713c3727dbb51b7ca265cc3a4045798
Joe Gesualdo <joegesualdo@gmail.com>
2014-01-03 19:59:01 -0500

    Add :first and :last to Devise users parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+
   # ------------------------------------------------------------------------
   # Note: Unable to access current user using Devise's 'current_user' method
   #       so created my own accessing the warden session object.
@@ -20,4 +23,13 @@ class ApplicationController < ActionController::Base
   def user_signed_in?
     current_user.present?
   end
+
+  protected
+
+  # permit additional parameters (the lazy way™) 
+  # TODO: Change Devise defaults and invoke custom behaviour so it ins't done lazily
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:first, :last]
+  end
+
 end
